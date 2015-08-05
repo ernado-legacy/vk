e0fd7e5b8ba554d01fcb5474f4afb02a66e17eb6
Александр Разумов <arazumov@ddestiny.ru>
2015-08-05 19:39:49 +0300

    added vk bool value

--- groups.go
+++ groups.go
@@ -1,4 +1,8 @@
 package vk
+import (
+	"io"
+	"errors"
+)
 
 type Groups struct {
 	factory RequestFactory
@@ -8,6 +12,47 @@ type Groups struct {
 const (
 	methodGroupsGetMembers = "groups.getMembers"
 )
+
+// Bool is special format for vk bool values
+// that are represented as integers - 1,0
+type Bool bool
+
+const (
+	byteOne = 49
+	byteZero = 48
+)
+
+func (v Bool) MarshalJSON() ([]byte, error) {
+	if v {
+		return []byte{byteOne}, nil
+	}
+	return []byte{byteZero}, nil
+}
+
+func (v *Bool) UnmarshalJSON(data []byte) error {
+	if data == nil || len(data) == 0 {
+		return nil
+	}
+	if len(data) != 1 {
+		return io.ErrUnexpectedEOF
+	}
+	if data[0] == byteOne {
+		*v = true
+	} else if data[0] == byteZero {
+		*v = false
+	} else {
+		return errors.New("bool value overflow")
+	}
+	return nil
+}
+
+type Group struct {
+	ID       int    `json:"id"`
+	Name     string `json:"name`
+	IsClosed Bool   `json:"is_closed"`
+	IsAdmin  Bool   `json:"is_admin"`
+	IsMember Bool   `json:"is_member"`
+}
 
 type GroupSearchFields struct {
 	ID int
--- groups_test.go
+++ groups_test.go
@@ -39,5 +39,54 @@ func TestGroups(t *testing.T) {
 		So(members.Items[0].Sex, ShouldEqual, Male)
 		user := members.Items[0]
 		So(user.Country.Is(Russia), ShouldBeTrue)
+		Convey("JSON", func() {
+			Convey("Marshal", func(){
+				v := struct {
+					Value Bool
+				}{true}
+				data, err := json.Marshal(v)
+				So(err, ShouldBeNil)
+				sData := bytes.NewBuffer(data).String()
+				So(sData, ShouldEqual, `{"Value":1}`)
+
+				v2 := struct {
+					Value Bool
+				}{false}
+				data, err = json.Marshal(v2)
+				So(err, ShouldBeNil)
+				sData = bytes.NewBuffer(data).String()
+				So(sData, ShouldEqual, `{"Value":0}`)
+				Convey("Errors", func() {
+					v := new(Bool)
+					So(v.UnmarshalJSON([]byte("1")), ShouldBeNil)
+					So(*v, ShouldEqual, true)
+
+					So(v.UnmarshalJSON([]byte("0")), ShouldBeNil)
+					So(*v, ShouldEqual, false)
+
+					So(v.UnmarshalJSON([]byte("-1")), ShouldNotBeNil)
+					So(v.UnmarshalJSON([]byte("9")), ShouldNotBeNil)
+
+					So(v.UnmarshalJSON([]byte{}), ShouldBeNil)
+					So(*v, ShouldEqual, false)
+				})
+			})
+			Convey("True", func() {
+				data := []byte(`
+				{"is_closed": 1}
+				`)
+				value := Group{}
+				So(json.Unmarshal(data, &value), ShouldBeNil)
+				So(value.IsClosed, ShouldEqual, true)
+			})
+			Convey("False", func() {
+				data := []byte(`
+				{"is_closed": 0}
+				`)
+				value := Group{}
+				So(json.Unmarshal(data, &value), ShouldBeNil)
+				So(value.IsClosed, ShouldEqual, false)
+			})
+		})
 	})
 }
